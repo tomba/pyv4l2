9aea6cbb1d28f34f9a662852c762558672bb63fc
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2023-12-10 15:40:43 +0200

    Linting, README

---
+++ README.md
@@ -0,0 +1,11 @@
+[![Lint Status](https://github.com/tomba/pyv4l2/actions/workflows/pylint.yml/badge.svg)](https://github.com/tomba/pyv4l2/actions/workflows/pylint.yml)
+
+# Pure-Python Linux kernel Video4Linux2 (V4L2) bindings
+
+## v4l2.uapi
+
+v4l2.uapi namespace contains the kernel user-space API (uAPI).
+
+## v4l2
+
+v4l2 namespace contains wrappers to the uAPI to simplify the use of the uAPI.
--- gen.sh
+++ gen.sh
@@ -23,3 +23,6 @@ ${CTYPESGEN} ${CTYPESGEN_OPTS} -I${INCLUDE_PATH} -o ${OUT} ${INCLUDES}
 
 # Fix _IOC by using ord(type)
 sed --in-place s#"return ((((dir << _IOC_DIRSHIFT) | (type << _IOC_TYPESHIFT)) | (nr << _IOC_NRSHIFT)) | (size << _IOC_SIZESHIFT))"#"return ((((dir << _IOC_DIRSHIFT) | (ord(type) << _IOC_TYPESHIFT)) | (nr << _IOC_NRSHIFT)) | (size << _IOC_SIZESHIFT))"# ${OUT}
+
+# Add pylint ignore comment
+sed --in-place s/"^def POINTER(obj):"/"def POINTER(obj): # pylint: disable=function-redefined:"/ v4l2/uapi/ctypes_preamble.py
