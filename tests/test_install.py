af615df7d07110a1b93f100ba9027bfb4df717dc
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2024-08-22 13:40:10 +0300

    Add install stuff

---
+++ tests/test_install.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+import unittest
+import v4l2
+
+class TestInstall(unittest.TestCase):
+    def test_install(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
