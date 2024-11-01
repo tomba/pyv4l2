tomba/pyv4l2

commit 35f4a0034eb581c9c19f8813f0afa3f37e263e71

utils: add Stream typing

utils/cam.py
@@ -15,6 +15,8 @@ from typing import TYPE_CHECKING
 
 from cam_helpers import read_config, NetTX, save_fb_to_file, disable_all_links, configure_subdevs, setup_links
 
+from cam_types import Stream
+
 import v4l2
 from v4l2.videodev import VideoCaptureStreamer
 
@@ -26,9 +28,9 @@ class Context:
     config: dict
     use_ipython: bool
     user_script: types.ModuleType | None
-    subdevices: dict
-    streams: list
-    md: v4l2.MediaDevice
+    subdevices: dict[str, v4l2.SubDevice] | None
+    streams: list[Stream]
+    md: v4l2.MediaDevice | None
     buf_type: str
     use_display: bool
     kms_committed: bool
@@ -153,15 +155,19 @@ def init_viddevs(ctx: Context):
         stream['display'] = ctx.use_display and stream.get('display', True)
         stream['embedded'] = stream.get('embedded', False)
 
-        if len(stream['fmt']) == 3:
-            stream['w'] = stream['fmt'][0]
-            stream['h'] = stream['fmt'][1]
-            stream['format'] = stream['fmt'][2]
+        fmt = stream['fmt']
+        if len(fmt) == 3:
+            stream['w'] = fmt[0]
+            stream['h'] = fmt[1]
+            stream['format'] = fmt[2]
         elif len(stream['fmt']) == 2:
-            stream['size'] = stream['fmt'][0]
-            stream['format'] = stream['fmt'][1]
+            stream['w'] = fmt[0]
+            stream['h'] = 1
+            stream['format'] = fmt[1]
         else:
             raise RuntimeError()
+
+        stream['size'] = stream['w'] * stream['h']
 
         if ctx.md:
             vid_ent = ctx.md.find_entity(stream['entity'])
@@ -198,14 +204,11 @@ def init_streamer(ctx: Context):
         mem_type = v4l2.MemType.DMABUF if ctx.buf_type == 'drm' else v4l2.MemType.MMAP
 
         if not stream.get('embedded', False):
+            assert isinstance(stream['format'], v4l2.PixelFormat)
             cap = vd.get_capture_streamer(mem_type, stream['w'], stream['h'], stream['format'])
         else:
-            if 'size' in stream:
-                size = stream['size']
-            else:
-                size = (stream['w'], stream['h'])
-
-            cap = vd.get_meta_capture_streamer(mem_type, size, stream['format'])
+            assert isinstance(stream['format'], v4l2.MetaFormat)
+            cap = vd.get_meta_capture_streamer(mem_type, stream['size'], stream['format'])
 
         stream['cap'] = cap
 
@@ -260,7 +263,7 @@ def setup(ctx: Context):
         ctx.updater = None
 
 
-def queue_buf(ctx: Context, stream, vbuf: v4l2.VideoBuffer):
+def queue_buf(ctx: Context, stream: Stream, vbuf: v4l2.VideoBuffer):
     cap = stream['cap']
     cap.queue(vbuf)
 
@@ -288,7 +291,7 @@ def handle_sent_buffers(ctx: Context):
         queue_buf(ctx, stream, vbuf)
 
 
-def readvid(ctx: Context, stream):
+def readvid(ctx: Context, stream: Stream):
     if ctx.verbose:
         print('{}: event'.format(stream['dev_path']))
 

utils/cam_types.py
@@ -1,0 +1,53 @@
+from __future__ import annotations
+
+from collections import deque
+from typing import TypedDict
+
+from kms import DumbFramebuffer, Plane
+from v4l2 import PixelFormat, MetaFormat
+
+from v4l2.videodev import CaptureStreamer, VideoBuffer, VideoDevice
+
+
+pix_or_meta_fmt = PixelFormat | MetaFormat
+
+
+Stream = TypedDict(
+    'Stream',
+    {
+        'id': int,
+        'num_bufs': int,
+        'display': bool,
+        'embedded': bool,
+        'fmt': tuple[int, int, pix_or_meta_fmt] | tuple[int, pix_or_meta_fmt],
+        'w': int,
+        'h': int,
+        'size': int,
+        'format': pix_or_meta_fmt,
+        'entity': str,
+        'dev_path': str,
+        'dev': VideoDevice,
+        'device': tuple[str, str],
+        'cap': CaptureStreamer,
+        'fbs': list[DumbFramebuffer],
+        'kms-buf-w': int,
+        'kms-buf-h': int,
+        'kms-format': PixelFormat,
+        'kms-src-w': int,
+        'kms-src-h': int,
+        'kms-src-x': int,
+        'kms-src-y': int,
+        'kms-dst-w': int,
+        'kms-dst-h': int,
+        'kms-dst-x': int,
+        'kms-dst-y': int,
+        'kms_old_fb': DumbFramebuffer | None,
+        'kms_fb': DumbFramebuffer,
+        'kms_fb_queue': deque,
+        'plane': Plane,
+        'total_num_frames': int,
+        'last_framenum': int,
+        'last_timestamp': float,
+        'tx_buf': VideoBuffer | None,
+    },
+)

utils/cam_helpers.py
@@ -11,6 +11,8 @@ from typing import TYPE_CHECKING
 import v4l2
 import v4l2.uapi
 
+from cam_types import Stream
+
 if TYPE_CHECKING:
     from .cam import Context
     import kms
@@ -67,7 +69,7 @@ class NetTX:
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
 
-    def tx(self, stream, vbuf, is_drm):
+    def tx(self, stream: Stream, vbuf, is_drm):
         cap = stream['cap']
 
         plane_sizes = cap.buffersizes
@@ -279,7 +281,8 @@ def configure_subdevs(ctx: Context, config):
 
     return subdevices
 
-def save_fb_to_file(stream, is_drm, fb_or_vbuf):
+
+def save_fb_to_file(stream: Stream, is_drm, fb_or_vbuf):
     cap = stream['cap']
 
     filename = 'frame-{}-{}.data'.format(stream['id'], stream['total_num_frames'])

utils/cam_kms.py
@@ -3,6 +3,9 @@ from selectors import BaseSelector, EVENT_READ
 import time
 
 import kms
+
+from cam_types import Stream
+
 import v4l2
 
 class KmsContext:
@@ -49,10 +52,11 @@ class KmsContext:
                 res.reserve_generic_plane(crtc, v4l2.PixelFormats.RGB565)
 
             # If we don't have a DRM fmt, just fall back to RGB565
-            if isinstance(stream['format'], v4l2.MetaFormat) or not stream['format'].drm_fourcc:
+            format = stream['format']
+            if isinstance(format, v4l2.MetaFormat) or not format.drm_fourcc:
                 stream['kms-format'] = v4l2.PixelFormats.RGB565
             else:
-                stream['kms-format'] = stream['format']
+                stream['kms-format'] = format
 
             if ctx.buf_type == 'drm' and stream.get('embedded', False):
                 divs = [16, 8, 4, 2, 1]
@@ -102,12 +106,14 @@ class KmsContext:
                 assert(plane)
                 stream['plane'] = plane
 
-    def alloc_fbs(self, stream):
+    def alloc_fbs(self, stream: Stream):
         fbs = []
         cap = stream['cap']
         for i in range(stream['num_bufs']):
             fb = kms.DumbFramebuffer(self.card, stream['kms-buf-w'], stream['kms-buf-h'], stream['kms-format'])
             fbs.append(fb)
+
+            assert isinstance(cap.format, v4l2.PixelFormat)
 
             for i in range(len(cap.format.planes)):
                 assert cap.strides[i] == fb.planes[i].pitch
@@ -115,7 +121,7 @@ class KmsContext:
 
         stream['fbs'] = fbs
 
-    def setup_stream(self, stream):
+    def setup_stream(self, stream: Stream):
         ctx = self.ctx
 
         assert(ctx.buf_type == 'drm')

commit af195e401e9a14d0669af56a9a80e001020d7e40

cam.py: Fix stream['size']
Fixes issue with embedded data.

Fixes: 35f4a00 ("utils: add Stream typing")
Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>

utils/cam.py
@@ -161,15 +161,13 @@ def init_viddevs(ctx: Context):
         if len(fmt) == 3:
             stream['w'] = fmt[0]
             stream['h'] = fmt[1]
+            stream['size'] = (stream['w'], stream['h'])
             stream['format'] = fmt[2]
-        elif len(stream['fmt']) == 2:
-            stream['w'] = fmt[0]
-            stream['h'] = 1
+        elif len(fmt) == 2:
+            stream['size'] = fmt[0]
             stream['format'] = fmt[1]
         else:
             raise RuntimeError()
-
-        stream['size'] = stream['w'] * stream['h']
 
         if ctx.md:
             vid_ent = ctx.md.find_entity(stream['entity'])

utils/cam_types.py
@@ -22,7 +22,7 @@ Stream = TypedDict(
         'fmt': tuple[int, int, pix_or_meta_fmt] | tuple[int, pix_or_meta_fmt],
         'w': int,
         'h': int,
-        'size': int,
+        'size': int | tuple[int, int],
         'format': pix_or_meta_fmt,
         'entity': str,
         'dev_path': str,