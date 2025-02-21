1780d222eda615c46c5acf6de35151d4493c0d4d
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2025-02-21 08:51:36 +0200

    cam: Combine NetTX and NetConsumer

--- utils/cam_net.py
+++ utils/cam_net.py
@@ -10,63 +10,13 @@ from v4l2 import MetaFormat
 
 from cam_types import Stream, Context, Consumer
 
-class NetTX:
+class NetConsumer(Consumer):
     # ctx-idx, width, height, strides[4], format[16], num-planes, plane[4]
     struct_fmt = struct.Struct('<III4I16pI4I')
 
-    def __init__(self, host: str, port: int) -> None:
+    def __init__(self, host: str, port: int):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.sock.connect((host, port))
-
-    def tx(self, stream: Stream, vbuf, is_drm):
-        cap = stream.cap
-
-        plane_sizes = cap.buffersizes
-        strides = cap.strides
-
-        # Extend lists to 4 elements
-        plane_sizes.extend(0 for _ in range(4 - len(plane_sizes)))
-        strides.extend(0 for _ in range(4 - len(strides)))
-
-        fmt = stream.format
-
-        if isinstance(fmt, MetaFormat):
-            num_planes = 1
-        else:
-            num_planes = len(fmt.planes)
-
-        hdr = NetTX.struct_fmt.pack(
-            stream.id,
-            stream.w,
-            stream.h,
-            *strides,
-            bytes(fmt.name, 'ascii'),
-            num_planes,
-            *plane_sizes,
-        )
-
-        self.sock.sendall(hdr)
-
-        if is_drm:
-            fb = next((fb for fb in stream.fbs if fb.fd(0) == vbuf.fd), None)
-            assert fb is not None
-
-            with mmap.mmap(fb.fd(0), fb.size(0), mmap.MAP_SHARED, mmap.PROT_READ) as b:
-                self.sock.sendall(b)
-        else:
-            # Need PROT_WRITE to be able to read fe-config buffers
-            with mmap.mmap(
-                cap.fd,
-                cap.framesize,
-                mmap.MAP_SHARED,
-                mmap.PROT_READ | mmap.PROT_WRITE,
-                offset=vbuf.offset,
-            ) as b:
-                self.sock.sendall(b)
-
-class NetConsumer(Consumer):
-    def __init__(self, host: str, port: int):
-        self.net_tx = NetTX(host, port)
         self.net_tx_queue = queue.Queue()
         self.net_done_queue = queue.Queue()
         self.net_thread = None
@@ -104,7 +54,7 @@ class NetConsumer(Consumer):
             if not data:
                 break
             stream, vbuf, is_drm = data
-            self.net_tx.tx(stream, vbuf, is_drm)
+            self.tx(stream, vbuf, is_drm)
             self.net_done_queue.put((stream, vbuf))
 
     def handle_tick(self, ctx: Context):
@@ -114,3 +64,49 @@ class NetConsumer(Consumer):
 
             cap = stream.cap
             cap.queue(vbuf)
+
+    def tx(self, stream: Stream, vbuf, is_drm):
+        cap = stream.cap
+
+        plane_sizes = cap.buffersizes
+        strides = cap.strides
+
+        # Extend lists to 4 elements
+        plane_sizes.extend(0 for _ in range(4 - len(plane_sizes)))
+        strides.extend(0 for _ in range(4 - len(strides)))
+
+        fmt = stream.format
+
+        if isinstance(fmt, MetaFormat):
+            num_planes = 1
+        else:
+            num_planes = len(fmt.planes)
+
+        hdr = NetConsumer.struct_fmt.pack(
+            stream.id,
+            stream.w,
+            stream.h,
+            *strides,
+            bytes(fmt.name, 'ascii'),
+            num_planes,
+            *plane_sizes,
+        )
+
+        self.sock.sendall(hdr)
+
+        if is_drm:
+            fb = next((fb for fb in stream.fbs if fb.fd(0) == vbuf.fd), None)
+            assert fb is not None
+
+            with mmap.mmap(fb.fd(0), fb.size(0), mmap.MAP_SHARED, mmap.PROT_READ) as b:
+                self.sock.sendall(b)
+        else:
+            # Need PROT_WRITE to be able to read fe-config buffers
+            with mmap.mmap(
+                cap.fd,
+                cap.framesize,
+                mmap.MAP_SHARED,
+                mmap.PROT_READ | mmap.PROT_WRITE,
+                offset=vbuf.offset,
+            ) as b:
+                self.sock.sendall(b)
