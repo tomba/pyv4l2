be9a021e146e2d68404d52bffe228207c9c8c106
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2026-04-20 08:15:54 +0300

    configs: opi5p hdmirx

---
+++ utils/cam/cam-configs/opi5p-hdmirx.py
@@ -0,0 +1,22 @@
+import v4l2
+
+w,h = 640, 480
+w,h = 1920, 1080
+fmt = v4l2.PixelFormats.RGB888
+
+fmt_pix = (w, h, fmt)
+
+configurations = {}
+
+configurations['hdmirx'] = {
+    'devices': [
+        {
+            'device': ('bus_info', 'platform:fdee0000.hdmi_receiver'),
+            'num_bufs': 5,
+            'fmt': fmt_pix,
+        },
+    ],
+}
+
+def get_configs():
+    return (configurations, ['hdmirx'])
