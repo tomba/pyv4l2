4c8da3b7f3322faeb656738ecae0b9c011834ae2
Tomi Valkeinen <tomi.valkeinen@ideasonboard.com>
2026-09-04 10:19:13 +0300

    tests: Add fd lifecycle tests

--- tests/test_hw.py
+++ tests/test_hw.py
@@ -16,6 +16,10 @@ class TestCardMethods(unittest.TestCase):
 
         return mdev
 
+    def _assert_closed(self, fd):
+        with self.assertRaises(OSError):
+            fcntl.fcntl(fd, fcntl.F_GETFD)
+
     def test_card(self):
         mdev = self._get_mdev()
 
@@ -23,8 +27,39 @@ class TestCardMethods(unittest.TestCase):
 
         mdev = None
         gc.collect()
-        with self.assertRaises(OSError):
-            fcntl.fcntl(fd, fcntl.F_GETFD)
+        self._assert_closed(fd)
+
+    def test_close(self):
+        mdev = self._get_mdev()
+        fd = mdev.fd
+
+        mdev.close()
+        self._assert_closed(fd)
+        self.assertEqual(mdev.fd, -1)
+
+        # A second close is a no-op
+        mdev.close()
+
+    def test_with(self):
+        with self._get_mdev() as mdev:
+            fd = mdev.fd
+
+        self._assert_closed(fd)
+
+    def test_subdev_and_videodev_close(self):
+        mdev = self._get_mdev()
+
+        for iface in mdev.interfaces:
+            if iface.is_subdev:
+                dev = v4l2.SubDevice(iface.dev_path)
+            elif iface.is_video:
+                dev = v4l2.VideoDevice(iface.dev_path)
+            else:
+                continue
+
+            fd = dev.fd
+            dev.close()
+            self._assert_closed(fd)
 
 
 if __name__ == '__main__':
